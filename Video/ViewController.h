111d634fae66e82ac36e368ebdf9dc1424753f2e
付州 <fu363128432@qq.com>
2016-08-23 01:28:26 +0800

    Initial Commit

---
+++ Video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Video
+//
+//  Created by 付州  on 16/8/23.
+//  Copyright © 2016年 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
